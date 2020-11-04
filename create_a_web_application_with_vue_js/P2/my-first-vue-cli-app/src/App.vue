aa911298e4d891de3e5c62734d39e791f0482dd5
roy <roy.framery@gmail.com>
2020-11-04 17:14:46 +0100

    Learned how to use component create a component and transfer data in components by using props

--- create_a_web_application_with_vue_js/P2/my-first-vue-cli-app/src/App.vue
+++ create_a_web_application_with_vue_js/P2/my-first-vue-cli-app/src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <nav class="navbar">
+    <NavLink url="/" text="Acceuil" />
+    <NavLink url="/about" text="À propos" />
+			<NavLink url="/contact" text="Contact" />
+  </nav>
   <HelloWorld msg="Welcome to my first vue cli app"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import NavLink from './components/NavLink.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    NavLink
   }
 }
 </script>
@@ -22,5 +29,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.navbar a{
+  margin-right: 10px;
 }
 </style>
---
+++ create_a_web_application_with_vue_js/P2/my-first-vue-cli-app/src/components/NavLink.vue
@@ -0,0 +1,19 @@
+// ./components/HomeLink.vue
+<template>
+	<a :href="url">{{ text }}</a> 
+</template>
+
+<script>
+export default {
+	name: 'NavLink',
+	props: ['url', 'text']
+}
+</script>
+
+<style scoped>
+	a {
+		text-decoration: none;
+        color: green;
+        font-weight: bold;
+	}
+</style>
